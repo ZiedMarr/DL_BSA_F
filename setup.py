ef60e459977d4aec67e195e6363a193209e4c342
ZiedMarr <zied.marrakchi2001@gmail.com>
2026-05-15 17:30:54 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DL_BSA_F",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "hydra-core",
+        "hydra-submitit-launcher",
+        "wandb",
+        "numpy",
+        # add your other dependencies
+    ],
+)
