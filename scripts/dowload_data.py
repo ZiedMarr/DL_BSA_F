537343451b77a5f614036def96508af560fb3033
ZiedMarr <zied.marrakchi2001@gmail.com>
2026-06-19 17:31:41 +0200

    correct path

--- scripts/dowload_data.py
+++ scripts/dowload_data.py
@@ -2,4 +2,4 @@ import os
 
 
 if __name__ == "__main__":
-    os.system("kaggle datasets download -d physionet/china-physiological-signal-challenge-in-2018 -p ./data --unzip")
+    os.system("kaggle datasets download -d physionet/china-physiological-signal-challenge-in-2018 -p ./data/raw --unzip")
