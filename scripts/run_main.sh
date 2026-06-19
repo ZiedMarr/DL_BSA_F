3e31dd3e0df817eb05113973f45848966020ccbb
unknown <claytonwongmozhe@gmail.com>
2026-06-19 13:46:25 +0200

    Edited run_main.sh and requirements.txt

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=ecg_training
-#SBATCH --partition=gpu-test
+#SBATCH --job-name=ecg_cnn_100ep
+#SBATCH --partition=gpu-teaching-2d
+#SBATCH --time=2-00:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --output=logs/job-%j.out
-#SBATCH --chdir=~/repo/DL_BSA_F
+#SBATCH --chdir=/home/bsa01/DL_BSA_F
 
-apptainer run --nv ~/containers/python_container.sif python main.py
+apptainer run --nv /home/bsa01/containers/python_container.sif python -u main.py --model cnn1d --epochs 100
