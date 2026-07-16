e8a04618d1cd2095e436196727eb1d494e3696e1
ZiedMarr <zied.marrakchi2001@gmail.com>
2026-07-16 11:48:19 +0200

    minor

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=ecg_cnn_100ep
+#SBATCH --job-name=ecg_cnnvit1_100ep
 #SBATCH --partition=gpu-teaching-2d
-#SBATCH --time=2-00:00:00
+#SBATCH --time=4-00:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --output=logs/job-%j.out
-#SBATCH --chdir=/home/bsa01/DL_BSA_F
+#SBATCH --chdir=/home/bsa05/DL_BSA_F
 
-apptainer run --nv ~/containers/bsa05_cont_3.sif python -u main.py --model cnn_vit --epochs 100
+apptainer run --nv /home/bsa05/containers/bsa05_cont_3.sif python -u main.py --model cnn_vit --epochs 100
