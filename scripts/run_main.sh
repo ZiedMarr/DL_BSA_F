55d6750f11880db0efa4cc7f429a83618e7af7be
Deep Learning for Biomedical Signal Analysis <bsa05@hydra.ml.tu-berlin.de>
2026-07-17 12:45:50 +0200

    add results

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=ecg_cnnvit1_100ep
 #SBATCH --partition=gpu-teaching-2d
-#SBATCH --time=4-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --output=logs/job-%j.out
